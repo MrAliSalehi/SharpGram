67e9224ffaf078210b0e09ae0185f3f9db5d3905
Ali <alisalehi498@yahoo.com>
2024-07-25 04:04:36 +0330

    console demo

--- Tests/SharpGram.Tests.Console/Program.cs
+++ Tests/SharpGram.Tests.Console/Program.cs
@@ -1,12 +1,12 @@
-﻿using EasyTg.Client;
-using Newtonsoft.Json.Linq;
-using SharpGram.Tl.Constructors.DcOptionNs;
+﻿using Newtonsoft.Json.Linq;
+using SharpGram.Client;
 
 var cts = new CancellationTokenSource();
 var secrets = (dynamic)JObject.Parse(await File.ReadAllTextAsync("devSecrets.json"));
 var apiId = int.Parse((string)secrets.api_id);
 var apiHash = (string)secrets.api_hash;
-
+var phone = (string)secrets.phone;
+var tfa = (string)secrets.two_factor;
 byte[] d = [];
 if (File.Exists("testSession"))
     d = await File.ReadAllBytesAsync("testSession");
@@ -14,22 +14,43 @@ if (File.Exists("testSession"))
 
 var tlSession = TelegramSession.LoadOrCreate(d);
 tlSession.ClientOptions.IsLocalServer = false;
-tlSession.ClientOptions.IsTest =true;
+tlSession.ClientOptions.IsTest = true;
+tlSession.ConnectionSession.IgnoreUpdates = true;
 
 tlSession.ApiId = apiId;
 tlSession.ApiHash = apiHash;
+tlSession.Phone = phone;
+tlSession.TwoFactorPassword = tfa;
 var client = new TelegramClient(tlSession, cts.Token);
-
 var result = await client.ConnectAsync();
-
 if (result.TryPickT1(out var err, out _))
 {
     Console.WriteLine($"err while trying to connect {err}");
+    return;
 }
 
-var conf = client.Session.Config;
+if (client.IsAuthorized(out var writer))
+{
+    Console.WriteLine("we are authorized");
+    return;
+}
 
-Console.WriteLine($"ready: {((DcOption)conf.DcOptions[0]).IpAddress}");
+
+Task.Run(async () =>
+{
+    Console.WriteLine("enter the code whenever you received it:");
+    var code = Console.ReadLine()!;
+    await writer.WriteAsync(code);
+});
+
+var authResult = await client.AuthorizeAsync();
+if (authResult.TryPickT1(out var authErr, out _))
+{
+    Console.WriteLine($"err on auth: {authErr}");
+    return;
+}
+
+Console.WriteLine("press any key");
 Console.ReadKey();
 
 var data = client.Session.Save();
